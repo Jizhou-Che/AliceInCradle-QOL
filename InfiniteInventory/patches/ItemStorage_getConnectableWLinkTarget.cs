a9f19bdbee70c544a128928d1db7f121ce6d9bbb
Jizhou-Che <che_mail@icloud.com>
2026-09-02 03:35:29 +0800

    Fixed an issue where newly obtained liquids fail to link to empty bottles in bottle holders when inventory is maxed out.

---
+++ InfiniteInventory/patches/ItemStorage_getConnectableWLinkTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using nel;
+
+namespace InfiniteInventory.patches;
+
+[HarmonyPatch(typeof(ItemStorage), "getConnectableWLinkTarget")]
+public class ItemStorage_getConnectableWLinkTarget
+{
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var codeMatcher = new CodeMatcher(instructions);
+        codeMatcher.MatchForward(false, new CodeMatch(OpCodes.Ldarg_0), new CodeMatch(OpCodes.Ldfld), new CodeMatch(OpCodes.Ldarg_0), new CodeMatch(OpCodes.Ldfld), new CodeMatch(OpCodes.Blt));
+        var label = codeMatcher.InstructionAt(4).operand;
+        codeMatcher.RemoveInstructions(5);
+        codeMatcher.Insert(new CodeInstruction(OpCodes.Br, label));
+        return codeMatcher.InstructionEnumeration();
+    }
+}
